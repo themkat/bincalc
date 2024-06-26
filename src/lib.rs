11041c14b4d95e1202f80145cd6fb75fd38f024d
Marie Katrine Ekeberg <mke@themkat.net>
2024-06-26 20:24:22 +0200

    Implement basic functionality for converting and toggling number systems

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
 use leptos::{
-    component, create_signal, event_target_value, svg::view, view, For, IntoView, SignalGet,
-    SignalSet,
+    component, create_signal, event_target_value, view, Callable, Callback, For, IntoView,
+    ReadSignal, SignalGet, SignalSet,
 };
+use radix_fmt::radix;
+
+// TODO: might be useful to limit the input somehow? hex numbers suddenly get big in binary lol :smile:
+//       would be fun to find a general way to calculate a number like that :D
+
+// TODO: maybe some sort of sorting could be user friendly?
 
 #[component]
 pub fn App() -> impl IntoView {
@@ -13,33 +19,67 @@ pub fn App() -> impl IntoView {
         <input type="text"
             prop:value=input
             on:input=move |ev| {
-                // TODO: validate?
-
                 let number = event_target_value(&ev);
                 set_input.set(number.clone());
                 set_valid.set(number_is_valid(number, base.get()));
             } />
         <label>"Valid: " {valid}</label>
-        <OutputList />
+        <OutputList number=move |_| u32::from_str_radix(input.get().as_str(), base.get()).unwrap_or(0) />
+    }
+}
+
+// placeholder struct
+struct NothingNess;
+
+#[component]
+fn OutputList(#[prop(into)] number: Callback<NothingNess, u32>) -> impl IntoView {
+    // TODO: maybe change to set. even if we only render each once due to the clever keys, we can probably still fill memory
+    let (selected_bases, set_selected_bases) = create_signal(vec![2, 10, 16]);
+
+    view! {
+        <ul>
+            <For each=move || selected_bases.get()
+            key=move |base| format!("{}-{}", number.call(NothingNess {}), base)
+            children=move |base| {
+                view! {
+                    <li>{base_name(base)} ": " {format!("{:#}", radix(number.call(NothingNess {}), base as u8))}</li>
+                }
+            }/>
+        </ul>
+        <BaseSelector selected_bases=selected_bases
+            on_selected=move |base| {
+                set_selected_bases.set(selected_bases.get().into_iter().chain(vec![base].into_iter()).collect())
+            }/>
     }
 }
 
 #[component]
-fn OutputList() -> impl IntoView {
-    let (selected_bases, set_selected_bases) = create_signal(vec![2, 10, 16]);
-    // TODO: plus sign at the bottom to add a new one
+fn BaseSelector(
+    selected_bases: ReadSignal<Vec<u32>>,
+    #[prop(into)] on_selected: Callback<u32>,
+) -> impl IntoView {
+    let (base, set_base) = create_signal(3_u32);
 
     view! {
-        <p>"Results will end up here"</p>
-        <ul>
-            <For each=move || selected_bases.get()
-            key=|base| base.clone()
-            children=move |base| {
-                view! {
-                    <p>{base_name(base)} ": "</p>
-                }
-            }/>
-        </ul>
+        <select name="Base" on:change=move |ev| {
+            set_base.set(event_target_value(&ev).parse().unwrap());
+        }
+                prop:value=move || base.get().to_string()
+        >
+            <For each=move || {
+                (2..=32).filter(|base| !selected_bases.get().contains(base)).collect::<Vec<u32>>()
+            }
+                 key=|base| base.clone()
+                 children=move |base| {
+                     view! {
+                         <option value=base>{base_name(base)}</option>
+                     }
+                 }
+             />
+        </select>
+        <button on:click=move |_| {
+            on_selected.call(base.get());
+        }>"Add"</button>
     }
 }
 
@@ -52,6 +92,7 @@ fn base_name(base: u32) -> Option<String> {
         2 => Some("Binary".to_string()),
         8 => Some("Octal".to_string()),
         10 => Some("Decimal".to_string()),
+        // TODO: 12 (Duodecimal)?
         16 => Some("Hexadecimal".to_string()),
         2..=32 => Some(format!("Base{base}")),
         _ => None,
